0b5277c9730c281772cd3cfe8a8b2ff4a9855436
sajjadulBappy <sajjadulkore@gmail.com>
2024-07-15 22:18:43 +0700

    problem solved at find firs string

--- problems_easy/find_first_string.dart
+++ problems_easy/find_first_string.dart
@@ -1,39 +1,22 @@
 class Solution {
   int strStr(String haystack, String needle) {
-      int tempLen = 0;
-      int haystackLen = haystack.length;
-      int firstIndex = 0;
-      bool isFirstIndex = true;
-      
-      for(int i = 0; i < haystackLen; i++){
-        if(needle[tempLen] == haystack[i]){
-          tempLen++;
-          if(isFirstIndex){
-            firstIndex = i;
-            isFirstIndex = false;
-          }
-        }else{
-          isFirstIndex = true;
-          tempLen = 0;
-          if(needle[tempLen] == haystack[i]){
-            tempLen++;
-            if(isFirstIndex){
-              firstIndex = i;
-              isFirstIndex = false;
-            }
-          }
-        }
-        
-        if(tempLen == needle.length){
-          return firstIndex;
-        }
-        
-      }
-      return -1;
+    int loopLen = haystack.length - needle.length;
+    int needlen = needle.length;
+    String checkString = haystack.substring(0, needlen);
+
+    if(checkString == needle){
+      return 0;
+    }
+    for(int i = 1; i < loopLen+1; i++){
+      checkString = haystack.substring(i, i+needlen);
+      if(checkString == needle) return i;
+    }
+    return -1;
+    
   }
 }
 
 void main(){
   Solution obj = Solution();
-  print(obj.strStr("leetcode", "code")); //"mississippi" , "issip" = 4 but this code output -1 this is wrong....
+  print(obj.strStr("mississippi", "issip")); //"mississippi" , "issip" = 4 but this code output -1 this is wrong....
 }
